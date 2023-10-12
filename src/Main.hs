d7f5cd89386a4efd40f483b446b7ad1855a4db3a
Celso Bonutti <i.am@cel.so>
2023-10-12 11:37:51 -0300

    js exports

--- src/Main.hs
+++ src/Main.hs
@@ -1,38 +1,39 @@
+{-# LANGUAGE DataKinds #-}
+
 module Main where
 
-import qualified Data.Aeson as A
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Unsafe as BU
-import Data.Text
-import Data.Text.Encoding
-import qualified Data.Text.Lazy.Encoding as Encoding
-import qualified ElmFormat.Cli as Cli
-import Foreign hiding (void)
-import Foreign.C.Types
-import qualified System.Environment
+import AST.Module (Module)
+import AST.Structure
+import AST.V0_16
+import qualified Data.Text as Text
+import qualified ElmFormat.AST.PublicAST as PublicAST
+import qualified ElmFormat.Parse as Parse
+import qualified ElmFormat.Render.Text as Render
+import ElmVersion
+import GHC.JS.Foreign.Callback
+import GHC.JS.Prim
+import Reporting.Annotation (Located)
+import qualified Reporting.Result as Result
+
+foreign import javascript "((f) => { formatFn = f })"
+    setF :: Callback (JSVal -> IO JSVal) -> IO ()
 
 main :: IO ()
-main =
-    mempty
-
--- marshalling
-
-foreign export ccall mallocPtr :: IO (Ptr (Ptr a))
-
-mallocPtr :: IO (Ptr (Ptr a))
-mallocPtr = malloc
-
-foreign export ccall formatRaw :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
-
-formatRaw :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
-formatRaw inputPtr inputLen outputPtrPtr = do
-    input <-
-        decodeUtf8 <$> BU.unsafePackMallocCStringLen (inputPtr, inputLen)
-    case encodeUtf8 <$> Cli.formatSimple input of
-        Left err -> pure (-1)
-        Right outputBytes ->
-            BU.unsafeUseAsCStringLen outputBytes \(buf, len) -> do
-                outputPtr <- mallocBytes len
-                poke outputPtrPtr outputPtr
-                copyBytes outputPtr buf len
-                pure len
+main = do
+    x <- syncCallback1' (return . toJSString . Text.unpack . format . Text.pack . fromJSString)
+    setF x
+  where
+    format input =
+        case Render.render Elm_0_19 <$> parseModule Elm_0_19 ("", input) of
+            Right output -> output
+            Left err -> Text.pack "error"
+    parseModule ::
+        ElmVersion ->
+        (FilePath, Text.Text) ->
+        Either () (Module [UppercaseIdentifier] (ASTNS Located [UppercaseIdentifier] 'TopLevelNK))
+    parseModule elmVersion (inputFile, inputText) =
+        case Parse.parse elmVersion inputText of
+            Result.Result _ (Result.Ok modu) ->
+                Right modu
+            Result.Result _ (Result.Err errs) ->
+                Left ()
